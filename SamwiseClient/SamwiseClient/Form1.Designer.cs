6c1337c31d47d2876b2feb7c7fe76078adb0bf24
Alexey Novikov <goremika@gmail.com>
2019-02-03 18:23:13 +0300

    research commit. pc identification
    PC can be identified by three params: mac address, Windows system PC SID, computer name.

--- SamwiseClient/SamwiseClient/Form1.Designer.cs
+++ SamwiseClient/SamwiseClient/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SamwiseClient
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.debugLog = new System.Windows.Forms.RichTextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // debugLog
+            // 
+            this.debugLog.Location = new System.Drawing.Point(12, 12);
+            this.debugLog.Name = "debugLog";
+            this.debugLog.Size = new System.Drawing.Size(776, 397);
+            this.debugLog.TabIndex = 0;
+            this.debugLog.Text = "";
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(618, 415);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(170, 23);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "Start";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.run_button_click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.debugLog);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox debugLog;
+        private System.Windows.Forms.Button runButton;
     }
 }
 
--- SamwiseClient/SamwiseClient/Form1.cs
+++ SamwiseClient/SamwiseClient/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Principal;
+using System.DirectoryServices;
+using System.Net.NetworkInformation;
+
+
 
 namespace SamwiseClient
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+
+        public static SecurityIdentifier GetComputerSid()
+        {
+            return new SecurityIdentifier((byte[])new DirectoryEntry(string.Format("WinNT://{0},Computer", Environment.MachineName)).Children.Cast<DirectoryEntry>().First().InvokeGet("objectSID"), 0).AccountDomainSid;
+        }
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void run_button_click(object sender, EventArgs e)
+        {
+            debugLog.Clear();
+            debugLog.AppendText("Start running\n");
+
+            var sid = GetComputerSid();
+            var macs = "";
+            var pc_name = System.Environment.MachineName;
+            var user_name = System.Security.Principal.WindowsIdentity.GetCurrent().Name; 
+
+            debugLog.AppendText("SID: "+sid+"\n");
+            debugLog.AppendText("PC name: " + pc_name + "\n");
+            debugLog.AppendText("User Name: " + user_name + "\n");
+
+
+
+
+
+
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                string ips = "";
+
+                foreach (UnicastIPAddressInformation ip in nic.GetIPProperties().UnicastAddresses)
+                {
+                    ips += ip.Address.ToString() + " ";
+                }
+
+
+                debugLog.AppendText(
+                    "Found MAC Address: " + nic.GetPhysicalAddress() + "\n" +
+                    "IP: " + ips + "\n" +
+                    "Description: " + nic.Description + "\n" +
+                    "Type: " + nic.NetworkInterfaceType+"\n");
+               
+            }
+
+
+
+
+
+
+
         }
     }
 }
